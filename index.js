23d85eb024085cd53d4093a39820c9f9b569b840
mytharcher <mytharcher@gmail.com>
2018-09-06 11:01:11 +0800

    Expose internal SearchInput component to public.

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ export { default as DataForm } from './lib/DataForm';
 export { default as FormDialog } from './lib/FormDialog';
 export { default as DataView } from './lib/DataView';
 export { default as GroupEditor } from './lib/GroupEditor';
+export { default as SearchInput } from './lib/SearchInput';
 export { default as SwitchGroup } from './lib/SwitchGroup';
 export { default as TagSelect } from './lib/TagSelect';
